c4bde0657901e7d7cf19267862d7b3ea2f820763
DARIUSISTOC1 <dariusistoc55@gmail.com>
2025-10-18 18:44:20 +0200

    Fix echo formatting in CMD-MENUv2.bat

--- CMD-MENUv2.bat
+++ CMD-MENUv2.bat
@@ -6,10 +6,10 @@ echo  *
 echo  /CCCCCC\   MMM\  /MMM  DDDDDDD\          MMM\  /MMM  EEEEEEEE  NN\     NN  UU      UU                /222222\
 echo  CCCCCCCCC  MMMM\/MMMM  DDDDDDDD\         MMMM\/MMMM  EEEEEEEE  NNN\    NN  UU      UU                22222222
 echo  CC/        MM\MMMM/MM  DD   \DDD         MM\MMMM/MM  EE        NNNN\   NN  UU      UU    VV\   /VV   2/   /22
-echo  CC         MM \MM/ MM  DD    \DD   ---   MM \MM/ MM  EEEEEEEE  NN NN\  NN  UU      UU    \VV   VV/      /22/
-echo  CC\        MM  \/  MM  DD     DD         MM  \/  MM  EE        NN  NN\ NN  UU\    /UU     VV\ /VV     /22/
-echo  CCCCCCCCC  MM      MM  DDDDDDDDD         MM      MM  EEEEEEEE  NN   NN\NN  UUUUUUUUUU     \VV VV/    22222222
-echo  \CCCCCC/   MM      MM  DDDDDDDDD         MM      MM  EEEEEEEE  NN    NNNN  \UUUUUUUU/      \VVV/     22222222
+echo  CC         MM \MM/ MM  DD    \DD   ---   MM \MM/ MM  EEEEEEEE  NN\NN\  NN  UU      UU    \VV   VV/      /22/
+echo  CC\        MM  \/  MM  DD     DD         MM  \/  MM  EE        NN \NN\ NN  UU\    /UU     VV\ /VV     /22/
+echo  CCCCCCCCC  MM      MM  DDDDDDDDD         MM      MM  EEEEEEEE  NN  \NN\NN  UUUUUUUUUU     \VV VV/    22222222
+echo  \CCCCCC/   MM      MM  DDDDDDDDD         MM      MM  EEEEEEEE  NN   \NNNN  \UUUUUUUU/      \VVV/     22222222
 echo  *                                                            
 echo  *  Version 2.1.5 du 16.10.2025 
 echo  *
